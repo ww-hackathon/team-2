5f2936a1e432914bebf2d51cf029857226559faa
Ratnadeep Rajendra Kharade <92khra1mst@hft-stuttgart.de>
2020-09-24 21:16:33 +0200

    Created reservation delete confirmation dialog

---
+++ src/main/webapp/app/reservations/all-reservations/reservation-delete-dialog/reservation-delete-dialog.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { JhiEventManager } from 'ng-jhipster';
+import { AllReservationService } from '../all-reservation.service';
+/* eslint-disable */
+@Component({
+  selector: 'jhi-reservation-delete-dialog',
+  templateUrl: './reservation-delete-dialog.component.html',
+  styleUrls: ['./reservation-delete-dialog.component.scss'],
+})
+export class ReservationDeleteDialogComponent {
+  id?: any;
+
+  constructor(
+    public activeModal: NgbActiveModal,
+    protected eventManager: JhiEventManager,
+    public allReservationService: AllReservationService
+  ) {}
+
+  cancel(): void {
+    this.activeModal.dismiss();
+  }
+
+  confirmDelete(): void {
+    console.log(this.id);
+    this.allReservationService.cancelReservation(this.id);
+    this.activeModal.dismiss();
+  }
+}
