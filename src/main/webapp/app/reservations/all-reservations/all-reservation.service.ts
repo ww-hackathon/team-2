9abdf82fb652740f863d4815381fee58fa94387e
Ratnadeep Rajendra Kharade <92khra1mst@hft-stuttgart.de>
2020-09-24 21:19:34 +0200

    created service to handle all reservations data

---
+++ src/main/webapp/app/reservations/all-reservations/all-reservation.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { DailyReservationService } from 'app/entities/daily-reservation/daily-reservation.service';
+import { IDailyReservation } from 'app/shared/model/daily-reservation.model';
+import { map, switchMap } from 'rxjs/operators';
+/* eslint-disable */
+
+@Injectable({ providedIn: 'root' })
+export class AllReservationService {
+  public dataSource: MatTableDataSource<IDailyReservation>;
+
+  public dataSource$: Observable<void> = this.reservationService.query().pipe(
+    map((data: any) => {
+      this.dataSource = new MatTableDataSource(data.body as IDailyReservation[]);
+    })
+  );
+  constructor(private reservationService: DailyReservationService) {
+    this.getReserVations();
+  }
+
+  getReserVations(): void {
+    this.dataSource$.subscribe();
+  }
+
+  cancelReservation(id: any): void {
+    this.reservationService
+      .delete(id!)
+      .pipe(
+        switchMap(() => {
+          return this.dataSource$;
+        })
+      )
+      .subscribe();
+  }
+}
