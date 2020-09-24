9c90007fd6ceef9e5f49963a6045483eaf2df810
Ratnadeep Rajendra Kharade <92khra1mst@hft-stuttgart.de>
2020-09-24 21:50:20 +0200

    displayed no data message on all reservations if there are no reservations

--- src/main/webapp/app/reservations/all-reservations/all-reservations.component.ts
+++ src/main/webapp/app/reservations/all-reservations/all-reservations.component.ts
@@ -1,11 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatSort } from '@angular/material/sort';
-import { MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { DailyReservationService } from 'app/entities/daily-reservation/daily-reservation.service';
 import { IDailyReservation } from 'app/shared/model/daily-reservation.model';
-import { Observable } from 'rxjs';
-import { map, switchMap } from 'rxjs/operators';
 import { AllReservationService } from './all-reservation.service';
 import { ReservationDeleteDialogComponent } from './reservation-delete-dialog/reservation-delete-dialog.component';
 /* eslint-disable */
@@ -24,11 +19,7 @@ export class AllReservationsComponent implements OnInit {
     modalRef.componentInstance.id = element.id;
   }
 
-  constructor(
-    private reservationService: DailyReservationService,
-    protected modalService: NgbModal,
-    public allReservationService: AllReservationService
-  ) {}
+  constructor(protected modalService: NgbModal, public allReservationService: AllReservationService) {}
 
   ngOnInit(): void {
     this.allReservationService.getReserVations();
