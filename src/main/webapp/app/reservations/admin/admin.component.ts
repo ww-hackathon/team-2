d548aa0d44bbe6fe0f905b17d7cdbbbab1d6971a
Ratnadeep Rajendra Kharade <92khra1mst@hft-stuttgart.de>
2020-09-24 11:00:05 +0200

    connected service with component to get and update threshold values.

--- src/main/webapp/app/reservations/admin/admin.component.ts
+++ src/main/webapp/app/reservations/admin/admin.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ThresholdService } from './threshold.service';
+
 @Component({
   selector: 'jhi-admin',
   templateUrl: './admin.component.html',
   styleUrls: ['./admin.component.scss'],
 })
 export class ReservationsAdminComponent implements OnInit {
-  constructor() {}
+  thresholdValue: any = '';
 
-  ngOnInit(): void {}
+  constructor(protected thresholdService: ThresholdService) {}
+
+  ngOnInit(): void {
+    this.thresholdService.query().subscribe((res: any) => (this.thresholdValue = res.body || ''));
+  }
+
+  onThresholdInput(event: any): void {
+    this.thresholdValue = event.target.value;
+  }
+
+  updateThresholdValue(): void {
+    this.thresholdService.update(this.thresholdValue).subscribe((res: any) => {
+      //console.log(res);
+    });
+  }
 }
