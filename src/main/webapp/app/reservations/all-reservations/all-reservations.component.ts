f059d7ec6b7c26bb24c57528bee384ad78eb9a6b
mrvxk <81klma1bif@hft-stuttgart.de>
2020-09-24 17:03:31 +0200

    Add "Meine Reservierungen" functions

--- src/main/webapp/app/reservations/all-reservations/all-reservations.component.ts
+++ src/main/webapp/app/reservations/all-reservations/all-reservations.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { DailyReservationService } from 'app/entities/daily-reservation/daily-reservation.service';
+import { IDailyReservation } from 'app/shared/model/daily-reservation.model';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+/* eslint-disable */
 
 @Component({
   selector: 'jhi-all-reservations',
@@ -6,7 +13,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-reservations.component.scss'],
 })
 export class AllReservationsComponent implements OnInit {
-  constructor() {}
+  displayedColumns = ['building', 'floor', 'wing', 'deskgroup', 'date', 'action-buttons'];
 
-  ngOnInit(): void {}
+  dataSource: MatTableDataSource<IDailyReservation>;
+
+  dataSource$: Observable<void> = this.reservationService.query().pipe(
+    map(data => {
+       this.dataSource = new MatTableDataSource(data.body as IDailyReservation[]);
+    })
+  );
+
+  onDeleteReservationClick(element: IDailyReservation) {
+    console.log(element);
+    this.reservationService
+      .delete(element.id!)
+      .pipe(
+        switchMap(() => {
+          return this.dataSource$;
+        })
+      )
+      .subscribe();
+  }
+
+  constructor(private reservationService: DailyReservationService) {}
+
+  ngOnInit(): void {
+    this.dataSource$.subscribe();
+  }
 }
