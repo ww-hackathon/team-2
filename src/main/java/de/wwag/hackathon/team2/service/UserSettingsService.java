509a726b31f57444a2c4dbdd20758e68cb6c10b0
Phil <phil@kirchniawy.de>
2020-09-25 07:16:41 +0200

    fix(build): fixing java errors while compiling

--- src/main/java/de/wwag/hackathon/team2/service/UserSettingsService.java
+++ src/main/java/de/wwag/hackathon/team2/service/UserSettingsService.java
@@ -4,47 +4,45 @@ import de.wwag.hackathon.team2.domain.DailyReservation;
 import de.wwag.hackathon.team2.domain.User;
 import de.wwag.hackathon.team2.repository.DailyReservationRepository;
 import de.wwag.hackathon.team2.web.rest.errors.BadRequestAlertException;
+import java.util.List;
+import java.util.NoSuchElementException;
 import org.h2.message.DbException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-import java.util.NoSuchElementException;
-
 @Service
 @Transactional
 public class UserSettingsService {
+  private final Logger log = LoggerFactory.getLogger(UserSettingsService.class);
 
-    private final Logger log = LoggerFactory.getLogger(UserSettingsService.class);
+  private final DailyReservationRepository dailyReservationRepository;
 
-    private final DailyReservationRepository dailyReservationRepository;
+  public UserSettingsService(UserService userService, DailyReservationRepository dailyReservationRepository) {
+    this.dailyReservationRepository = dailyReservationRepository;
+  }
 
-    public UserSettingsService(UserService userService, DailyReservationRepository dailyReservationRepository) {
-        this.dailyReservationRepository = dailyReservationRepository;
-    }
+  public List<DailyReservation> getAllDailyReservationsForUser() {
+    return dailyReservationRepository.findByUserIsCurrentUser();
+  }
 
-    public List<DailyReservation> getAllDailyReservationsForUser() {
-        return dailyReservationRepository.findByUserIsCurrentUser();
-    }
+  public DailyReservation addDailyReservation(DailyReservation dailyReservation) {
+    return dailyReservationRepository.save(dailyReservation);
+  }
 
-    public DailyReservation addDailyReservation(DailyReservation dailyReservation) {
-        return dailyReservationRepository.save(dailyReservation);
-    }
+  public DailyReservation getDailyReservationById(Long id) {
+    return dailyReservationRepository.findById(id).<NoSuchElementException>orElseThrow(() -> new NoSuchElementException());
+  }
 
-    public DailyReservation getDailyReservationById(Long id)  {
-        return dailyReservationRepository.findById(id).orElseThrow(() -> {throw new NoSuchElementException();});
-    }
+  public void deleteDailyReservationById(Long id) {
+    dailyReservationRepository.deleteById(id);
+  }
 
-    public void deleteDailyReservationById(Long id) {
-        dailyReservationRepository.deleteById(id);
-    }
-
-    public DailyReservation updateDailyReservationById(Long id, DailyReservation updatedDailyReservation) {
-        DailyReservation dailyReservation = dailyReservationRepository.findById(id).orElseThrow(() -> {throw new NoSuchElementException();});
-        dailyReservation.setDate(updatedDailyReservation.getDate());
-        dailyReservation.setDeskgroup(updatedDailyReservation.getDeskgroup());
-        return dailyReservationRepository.save(dailyReservation);
-    }
+  public DailyReservation updateDailyReservationById(Long id, DailyReservation updatedDailyReservation) {
+    DailyReservation dailyReservation = dailyReservationRepository.findById(id).orElseThrow(() -> new NoSuchElementException());
+    dailyReservation.setDate(updatedDailyReservation.getDate());
+    dailyReservation.setDeskgroup(updatedDailyReservation.getDeskgroup());
+    return dailyReservationRepository.save(dailyReservation);
+  }
 }
